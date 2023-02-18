f95691d809732d356d91d5a05c3e5e0ac3c6dbe4
beulabeula <125734732+beulabeula@users.noreply.github.com>
2023-02-18 16:23:20 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,27 @@
+function focusname(){
+    document.getElementById('fname').style.border="1px solid red" ;
+    document.getElementById('name_error').style.display="block";
+
+   }
+function focusuname(){
+    document.getElementById('uname').style.border="1px solid red" ;
+    document.getElementById('uname_error').style.display="block";
+
+   }
+   function focuspwd(){
+    document.getElementById('pwd').style.border="1px solid red"; 
+    document.getElementById('pwd_error').style.display="block";
+
+   }
+   function focusmail(){
+    document.getElementById('email').style.border="1px solid red";
+    document.getElementById('mail_error').style.display="block";
+
+   }
+
+function focusnum(){
+    document.getElementById('mob').style.border="1px solid red"; 
+    document.getElementById('mobile_error').style.display="block";
+
+   }
+
